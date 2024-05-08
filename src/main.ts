0373f791ebfd45e0555aafb20fa587f6e67633be
袁磊（上海） <1452491917@qq.com>
2024-05-08 13:00:37 +0800

    feat: test

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routers'
-import pinia from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-import '@/styles/element-dark.scss'
-import './styles/index.scss'
-import './permission'
+import ElementPlus from "element-plus";
+import { createApp } from "vue";
+import pinia from "./store";
+import App from "./App.vue";
+import router from "./routers";
+import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/dark/css-vars.css";
+import "@/styles/element-dark.scss";
+import "./styles/index.scss";
+import "./permission";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.use(pinia)
-app.use(ElementPlus)
+app.use(router);
+app.use(pinia);
+app.use(ElementPlus);
 
-app.mount('#app')
+app.mount("#app");
