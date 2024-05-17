3ab1927faffde60a3b470fe83a1de2c012e2e9c3
袁磊（上海） <1452491917@qq.com>
2024-05-17 22:18:26 +0800

    feat: add list

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+  import UserSide from './components/userSide.vue'
+  import UserTable from './components/userTable.vue'
+</script>
 
 <template>
-  <div class="root">
-    list
+  <div class="app-container m-user">
+    <UserSide />
+    <UserTable />
   </div>
 </template>
 
 <style scoped lang="scss">
-@import './index.scss';
+  @import './index';
 </style>
